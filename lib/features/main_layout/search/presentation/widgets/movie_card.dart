d48f7a983cfb8d60948f7f64f397a52e5f367042
Zeinab Mansour <130790318+zeinabmhammed@users.noreply.github.com>
2025-12-16 00:21:51 +0200

    feat: Enable navigation to movie details in MovieCard
    This commit wraps the `MovieCard` widget with an `InkWell` to enable user interaction.
    
    Key changes:
    - **Navigation**: Tapping a `MovieCard` now navigates to the `MovieDetailsScreen`, passing the specific movie ID.
    - **Imports**: Added necessary imports for navigation and the destination screen.

--- lib/features/main_layout/search/presentation/widgets/movie_card.dart
+++ lib/features/main_layout/search/presentation/widgets/movie_card.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import '../../../../../core/resources/color_manger.dart';
 import '../../../../../core/resources/responsive/responsive.dart';
 import '../../../../../core/resources/styles_manger.dart';
+import '../../../../../core/routes_manger/routes.dart';
 import '../../../../../domain/entities/movie.dart';
+import '../../../../movie_details/presentation/screen/movie_details_screen.dart';
 
 class MovieCard extends StatelessWidget {
   final Movie movie;
@@ -13,50 +15,60 @@ class MovieCard extends StatelessWidget {
   Widget build(BuildContext context) {
     final responsive = Responsive(context);
 
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(16),
-      child: Stack(
-        children: [
-          AspectRatio(
-            aspectRatio: 183 / 279,
-            child: Image.network(
-              movie.posterPath,
-              fit: BoxFit.cover,
-              errorBuilder: (_, __, ___) => Container(
-                color: Colors.grey.shade300,
-                child: const Icon(Icons.broken_image, size: 40),
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => MovieDetailsScreen(movieId: movie.id),
+          ),
+        );
+      },
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+        child: Stack(
+          children: [
+            AspectRatio(
+              aspectRatio: 183 / 279,
+              child: Image.network(
+                movie.posterPath,
+                fit: BoxFit.cover,
+                errorBuilder: (_, __, ___) => Container(
+                  color: Colors.grey.shade300,
+                  child: const Icon(Icons.broken_image, size: 40),
+                ),
               ),
             ),
-          ),
-          Positioned(
-            top: 8,
-            left: 8,
-            child: Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: responsive.scaleWidth(6),
-                vertical: responsive.scaleHeight(4),
-              ),
-              decoration: BoxDecoration(
-                color: ColorManager.grey,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Row(
-                children: [
-                  Text(
-                    movie.rating.toString(),
-                    style: s16Regular(color: ColorManager.white),
-                  ),
-                  SizedBox(width: responsive.scaleWidth(4)),
-                  Icon(
-                    Icons.star,
-                    size: responsive.scaleWidth(12),
-                    color: ColorManager.yellow,
-                  ),
-                ],
+            Positioned(
+              top: 8,
+              left: 8,
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: responsive.scaleWidth(6),
+                  vertical: responsive.scaleHeight(4),
+                ),
+                decoration: BoxDecoration(
+                  color: ColorManager.grey,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Row(
+                  children: [
+                    Text(
+                      movie.rating.toString(),
+                      style: s16Regular(color: ColorManager.white),
+                    ),
+                    SizedBox(width: responsive.scaleWidth(4)),
+                    Icon(
+                      Icons.star,
+                      size: responsive.scaleWidth(12),
+                      color: ColorManager.yellow,
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
